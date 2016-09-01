e69982301b19f01ed2d5133d01bb0ee1c888c7dc
Matthew Herz <matthewaherz@gmail.com>
2016-09-01 13:04:14 -0700

    Add charmap example to start testing

---
+++ Example/SwiftSpriter/Examples/CharacterMaps.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterMaps.swift
+//  SwiftSpriter
+//
+//  Created by Matt on 9/1/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+class CharacterMaps: AnimationScene {
+    override func commonInit() {
+        self.fileName = "charmap"
+        self.entityName = "Male_high-res"
+        super.commonInit()
+    }
+}
